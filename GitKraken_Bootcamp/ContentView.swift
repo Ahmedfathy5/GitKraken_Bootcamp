6bdb2d634d06130c1014a590a6de0ff6e200a831
Lord Ahmed Fathy <ahmedfathyios1@gmail.com>
2024-05-17 01:18:09 +0300

    another commit

--- GitKraken_Bootcamp/ContentView.swift
+++ GitKraken_Bootcamp/ContentView.swift
@@ -14,11 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("ahmed fathy elsaka")
-            Button {
-                
-            } label: {
-                Text("Click me")
-            }
+         
 
         }
         .padding()
