8229cc25dbac41b08c67854458c8c11e7dec14b1
Lord Ahmed Fathy <ahmedfathyios1@gmail.com>
2024-05-17 01:24:30 +0300

    Add HomeView

---
+++ GitKraken_Bootcamp/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  GitKraken_Bootcamp
+//
+//  Created by Ahmed Fathi on 17/05/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
