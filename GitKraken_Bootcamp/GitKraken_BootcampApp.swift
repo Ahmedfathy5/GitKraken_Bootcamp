d156be5995a1f3cf972bab69f30a39b6b903e9df
Lord Ahmed Fathy <ahmedfathyios1@gmail.com>
2024-05-16 20:39:29 +0300

    Initial Commit

---
+++ GitKraken_Bootcamp/GitKraken_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitKraken_BootcampApp.swift
+//  GitKraken_Bootcamp
+//
+//  Created by Ahmed Fathi on 16/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitKraken_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
