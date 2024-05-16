7c7496f72517155e9895fd9592029a9a16f85427
Lord Ahmed Fathy <ahmedfathyios1@gmail.com>
2024-05-17 01:36:02 +0300

    Add a new text.

--- GitKraken_Bootcamp/ContentView.swift
+++ GitKraken_Bootcamp/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("ahmed fathy elsaka")
+            Text("what should i do this days 😏")
          
 
         }
